8e5aff1f0653c893d85e165d6cf4fd1e30885ac3
Olga.Noda <olga.noda@gmail.com>
2020-12-23 10:26:00 +0200

    added playing "rock, paper, scissors"

--- task231220.py
+++ task231220.py
@@ -107,3 +107,40 @@ elif discr == 0:
     print(f'Discriminant={discr}, x={x}')
 else:
     print('The equation has no solution')
+
+# task8
+'''
+камень-ножницы-бумага пока не надоест (Q) загадываем и спрашиваем пользователя что он загадал.
+Плюсуем себе или ему победу и так по кругу. Принтуем общий счет.
+'''
+import random
+quantity = int(input("Let's play rock, paper, scissors! Input how many times you want to play: "))
+t = 0
+comp_counter = 0
+player_counter = 0
+while t != quantity:
+    player = input('Choose rock, paper or scissors by typing r, p or s: ')
+    if player == 'r' or player == 'p' or player == 's':
+        computer = random.randint(1, 3)
+        if computer == 1: print(f'Computer: r')
+        elif computer == 2: print(f'Computer: p')
+        else: print(f'Computer: s')
+        if (computer == 1 and player == 'r' or computer == 2 and player == 'p' or computer == 3 and player == 's'):
+            comp_counter += 1
+            player_counter += 1
+            print(f'It is a draw. Score: computer {comp_counter} : player {player_counter}')
+        elif (computer == 1 and player == 'p' or computer == 2 and player == 's' or computer == 3 and player == 'r'):
+            player_counter += 1
+            print(f'You win! Score: computer {comp_counter} : player {player_counter}')
+        else:
+            comp_counter += 1
+            print(f'You lost. Score: computer {comp_counter} : player {player_counter}')
+    else:
+        print('Your input was in the wrong format')
+    t += 1
+if player_counter > comp_counter:
+    print(f'GAME OVER. Congratulations! You win! Score: computer {comp_counter} : player {player_counter}')
+elif player_counter < comp_counter:
+    print(f'GAME OVER. You lost. Score: computer {comp_counter} : player {player_counter}')
+else:
+    print(f'GAME OVER. It is a draw. Score: computer {comp_counter} : player {player_counter}')
