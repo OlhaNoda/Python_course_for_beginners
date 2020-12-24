ceabb0adfb86594fbf9d0c7d4256fbcd7cf8d9dc
Olga.Noda <olga.noda@gmail.com>
2020-12-24 22:46:47 +0200

    init commit

---
+++ lesson_261220/task1_261220.py
@@ -0,0 +1,14 @@
+# task1_261220
+'''
+The greatest number
+Write a Python program to get the largest number from a list of random numbers with the length of 10
+Constraints: use only while loop and random module to generate numbers
+'''
+import random
+random_list = []
+i = 0
+while i < 10:
+    random_list.append(random.randint(1, 10))
+    i += 1
+print(f'The list of random numbers: {random_list}')
+print(f'The largest number from a list: {max(random_list)}')
