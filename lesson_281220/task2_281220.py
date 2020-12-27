585eab3b16f14b6eca481176b58fcd52c29b9c47
Olga.Noda <olga.noda@gmail.com>
2020-12-27 22:06:34 +0200

    first version

---
+++ lesson_281220/task2_281220.py
@@ -0,0 +1,23 @@
+# task2_281220
+'''
+Computes and returns the total price of stock.
+'''
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+total_price = {}
+total = 0
+for key, value in stock.items():
+    total_price[key] = stock[key] * prices[key]
+    total = total + stock[key] * prices[key]
+print(f'total_price: {total_price} \ntotal: {total}')
+
