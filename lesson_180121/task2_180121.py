48e33b22097659ff6fe6049cd903a6c2ecc8f21c
onoda <olga.noda@gmail.com>
2021-01-17 19:44:51 +0200

    first version

--- lesson_180121/task2_180121.py
+++ lesson_180121/task2_180121.py
@@ -4,15 +4,12 @@ Task 2
 Write a Python program to access a function inside a function (Tips: use function, which returns another function)
 """
 
-from functools import reduce
 
-
-def make_sum_numbers_to_power_function(power):
+def make_sum_numbers_to_power(power):
     def sum_numbers(*numbers):
-        return reduce(lambda x, y: x+y**power, numbers)
+        return sum(num**power for num in numbers)
     return sum_numbers
 
 
 if __name__ == "__main__":
-    sum_numbers_to_power_2 = make_sum_numbers_to_power_function(2)
-    print(sum_numbers_to_power_2(1, 2, 3))
+    print(make_sum_numbers_to_power(2)(2, 3, 4))
