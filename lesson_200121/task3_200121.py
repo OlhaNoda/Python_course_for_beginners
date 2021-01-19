6fce1c06a885dd54d2603a364a8d360f60236026
onoda <olga.noda@gmail.com>
2021-01-19 20:18:24 +0200

    first version

---
+++ lesson_200121/task3_200121.py
@@ -0,0 +1,31 @@
+# task3_200121
+"""
+Write a decorator `arg_rules` that validates arguments passed to the function.
+A decorator should take 3 arguments:
+max_length: 15
+type_: str
+contains: [] - list of symbols that an argument should contain
+If some of the rules' checks returns False, the function should return False and print the reason it failed;
+otherwise, return the result.
+"""
+
+
+def arg_rules(type_: type, max_length: int, contains: list):
+    def chek_arg(f):
+        def wrapper(arg):
+            if isinstance(arg, type_) and len(arg) <= max_length:
+                return f(arg)
+            print(f'Arg must be {type_}\nMax length of arg is {max_length}\nArg should contain {contains}')
+            return False
+        return wrapper
+    return chek_arg
+
+
+@arg_rules(type_=str, max_length=15, contains=['05', '@'])
+def create_slogan(name: str):
+    return f"{name} drinks pepsi in his brand new BMW!"
+
+
+if __name__ == "__main__":
+    print(create_slogan('johndoegma'))
+
