c60ebebd13cc1d89588f522c571cd8c4c51c6d04
onoda <olga.noda@gmail.com>
2021-01-19 18:24:33 +0200

    first version

---
+++ lesson_200121/task1_200121.py
@@ -0,0 +1,29 @@
+# task1_200121
+"""
+Write a decorator that prints a function with arguments passed to it.
+NOTE! It should print the function, not the result of its execution!
+For example:
+"add called with 4, 5"
+"""
+
+
+def logger(func):
+    def wrapper(* args):
+        print(f'{func.__name__} called with {args}')
+        return func(*args)
+    return wrapper
+
+
+@logger
+def add(*args):
+    return sum(arg for arg in args)
+
+
+@logger
+def square_all(*args):
+    return [arg ** 2 for arg in args]
+
+
+if __name__ == "__main__":
+    add(1, 2)
+    square_all(3, 4, 5)
