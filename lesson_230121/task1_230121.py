69b654b668e55f34a91c659ee432cd2853e682bd
onoda <olga.noda@gmail.com>
2021-01-21 19:55:15 +0200

    first version

---
+++ lesson_230121/task1_230121.py
@@ -0,0 +1,30 @@
+# task1_230121
+"""
+Create a class method named `validate`, which should be called from the `__init__` method
+to validate parameter email, passed to the constructor.
+The logic inside the `validate` method could be to check if the passed email parameter is a valid email string.
+Email validations:
+https://help.xmatters.com/ondemand/trial/valid_email_format.htm
+https://en.wikipedia.org/wiki/Email_address
+"""
+
+
+class Person:
+    def __init__(self, name, email: str):
+        self.name = name
+        self.email = email
+        Person.validate_email(self.email)
+
+    @classmethod
+    def validate_email(cls, email):
+        answer = ''
+        if email.find('@') > 0:
+            answer = 'Valid'
+        else:
+            answer = 'Not valid'
+        return answer
+
+
+if __name__ == "__main__":
+    p = Person('Steve', 'etetet@ururu')
+    print(p.validate_email(p.email))
