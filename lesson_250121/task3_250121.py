73b1442acd6b7e635d32db57def8f86c9acb2f9f
onoda <olga.noda@gmail.com>
2021-01-25 17:20:25 +0200

    first version

--- lesson_250121/task3_250121.py
+++ lesson_250121/task3_250121.py
@@ -7,15 +7,15 @@ Also, add logic for retrieving elements using square brackets syntax.
 my_list = [5, 10, 15, 20]
 
 
-def for_in(iterable):
+def my_for_in(iterable):
     iterator = iter(iterable)
-    completed_iterating = False
-    while not completed_iterating:
+    iterating_finished = False
+    while not iterating_finished:
         try:
             print(next(iterator))
         except StopIteration:
-            completed_iterating = True
+            iterating_finished = True
 
 
 if __name__ == "__main__":
-    for_in(my_list)
+    my_for_in(my_list)
