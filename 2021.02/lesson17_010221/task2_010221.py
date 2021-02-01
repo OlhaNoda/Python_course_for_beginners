1445e661ae84fdf6efb844b4d1d2e1e9cd614cca
onoda <olga.noda@gmail.com>
2021-02-01 18:32:52 +0200

    first version

--- 2021.02/lesson17_010221/task2_010221.py
+++ 2021.02/lesson17_010221/task2_010221.py
@@ -6,16 +6,16 @@ phone_book = 'phone_book.json'
 
 class PhoneBookTestCase(unittest.TestCase):
 
-     def test_add_contact(self):
-         new_entry = {
+    def test_add_contact(self):
+        new_entry = {
              'name': 'Mykola',
              'last_name': 'Mykolenko',
              'phone': '74757575',
              'country': 'Ukraine',
              'city': 'Kyiv'
          }
-         app.add_contact(phone_book, new_entry)
-         self.assertIn(new_entry, app.read_file(phone_book))
+        app.add_contact(phone_book, new_entry)
+        self.assertIn(new_entry, app.read_file(phone_book))
 
     def test_delete_contact(self):
         deleted_entry = app.read_file(phone_book)[0]
