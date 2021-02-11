b6e172d0ea9a2c6f574c73d44bb3b79d6892dd58
onoda <olga.noda@gmail.com>
2021-02-12 00:53:14 +0200

    first version

--- 2021_02/lesson22_130221/task2_130221.py
+++ 2021_02/lesson22_130221/task2_130221.py
@@ -5,19 +5,19 @@ All tasks should be solved using recursion
 """
 
 
-def is_palindrome(looking_str: str) -> bool:
+def is_palindrome_recursive(looking_str: str) -> bool:
     if not isinstance(looking_str, str):
-        raise TypeError(f'This function {is_palindrome.__name__} works only with looking_str: str')
+        raise TypeError(f'This function {is_palindrome_recursive.__name__} works only with looking_str: str')
     if len(looking_str) < 1:
         return True
     if looking_str[0] == looking_str[-1]:
-        return is_palindrome(looking_str[1:-1])
+        return is_palindrome_recursive(looking_str[1:-1])
     else:
         return False
 
 
 if __name__ == "__main__":
-    assert is_palindrome('mom') == True
-    assert is_palindrome('sassas') == True
-    assert is_palindrome('0') == True
-    assert is_palindrome('qwerty') == False
+    assert is_palindrome_recursive('mom') == True
+    assert is_palindrome_recursive('sassas') == True
+    assert is_palindrome_recursive('0') == True
+    assert is_palindrome_recursive('qwerty') == False
---
+++ 2021_02/lesson22_130221/task4_130221.py
@@ -0,0 +1,19 @@
+# task4_130221
+"""
+def reverse(input_str: str) -> str:
+Function returns reversed input string
+All tasks should be solved using recursion
+"""
+
+
+def reverse_recursive(input_str: str) -> str:
+    if not isinstance(input_str, str):
+        raise TypeError(f'This function {reverse_recursive.__name__} works only with input_str: str')
+    if len(input_str) == 0:
+        return input_str
+    return reverse_recursive(input_str[1:])+input_str[0]
+
+
+if __name__ == "__main__":
+    assert reverse_recursive('hello') == 'olleh'
+    assert reverse_recursive('o') == 'o'
