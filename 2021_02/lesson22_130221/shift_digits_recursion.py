aa2f3264dda2bc3fac34af49fd38d885f590c23b
onoda <olga.noda@gmail.com>
2021-02-13 02:29:28 +0200

    first version

--- 2021_02/lesson22_130221/shift_digits_recursion.py
+++ 2021_02/lesson22_130221/shift_digits_recursion.py
@@ -31,9 +31,9 @@ def shift_digits_right_recursive(number: int, step: int = 1) -> int:
         return number
     last_digit = number % 10  # определение последней цифры в числе
     if last_digit == 0 and step != 1:  # если последняя цифра 0 и это не последний шаг
-        last_digits = number % 100  # определение последних двух цифр в числе
+        last_two_digits = number % 100  # определение последних двух цифр в числе
         number //= 100  # отбрасывание последних двух цифр из числа
-        number += last_digits * 10**(places-2)  # изменение числа - последние две цифры становятся первыми
+        number += last_two_digits * 10**(places-2)  # изменение числа - последние две цифры становятся первыми
         return shift_digits_right_recursive(number, step - 2)
     else:
         number //= 10  # отбрасывание последней цифры из числа
@@ -41,6 +41,29 @@ def shift_digits_right_recursive(number: int, step: int = 1) -> int:
         return shift_digits_right_recursive(number, step - 1)
 
 
+# Сдвиг цифр в числе влево на заданное количество шагов
+def shift_digits_left_recursive(number: int, step: int = 1) -> int:
+    if not isinstance(number, int) or not isinstance(step, int):
+        raise TypeError(f'The function {shift_digits_left_recursive.__name__} works only with number: int, step: int')
+    if step < 0:
+        raise ValueError(f'The function {shift_digits_left_recursive.__name__} works only with step >= 0')
+    places = count_places_recursive(number)
+    if step == 0:
+        return number
+    first_two_digits = number // 10**(places-2)  # определение первых двух цифр в числе
+    if first_two_digits % 10 == 0 and step != 1:  # если число из первых двух цифр кратно 10 и это не последний шаг
+        number %= 10**(places-2)  # отбрасывание первых двух цифр из числа
+        number = number*100 + first_two_digits  # изменение числа - первые две цифры становятся последними
+        return shift_digits_left_recursive(number, step - 2)
+    else:
+        first_digit = number // 10**(places-1)  # определение первой цифры в числе
+        number %= 10**(places-1)  # отбрасывание первой цифры из числа
+        number = number * 10 + first_digit  # изменение числа - первая цифра становится последней
+        return shift_digits_left_recursive(number, step - 1)
+
+
 if __name__ == "__main__":
     assert shift_digits_right_recursive(1035, 3) == 351
     assert shift_digits_right_recursive(1035, 4) == 1035
+    assert shift_digits_left_recursive(1035, 1) == 351
+    assert shift_digits_left_recursive(1035, 2) == 3510
