3e059ab59137a9a76af5bd580af7ebb26dc42bd5
onoda <olga.noda@gmail.com>
2021-03-01 16:40:04 +0200

    first version

--- 2021_03/lesson30_010321/task2_010321/caesar_cipher.py
+++ 2021_03/lesson30_010321/task2_010321/caesar_cipher.py
@@ -1,9 +1,12 @@
 def caesar(text: str, shift: int = 3) -> str:
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     result_text = ''
-    for letter in text.lower():
+    for letter in text:
         if letter.isalpha():
-            result_text += alpha[(alpha.index(letter) + shift) % len(alpha)]
+            if letter.islower():
+                result_text += alpha[(alpha.index(letter) + shift) % len(alpha)]
+            else:
+                result_text += alpha[(alpha.index(letter.lower()) + shift) % len(alpha)].upper()
         else:
             result_text += letter
     return result_text
