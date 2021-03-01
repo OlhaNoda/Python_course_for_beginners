e01da0aea22dc6cc3646c4dfe46e2ff14f9cf3f6
onoda <olga.noda@gmail.com>
2021-03-01 16:46:20 +0200

    first version

--- 2021_03/lesson30_010321/task2_010321/client.py
+++ 2021_03/lesson30_010321/task2_010321/client.py
@@ -5,7 +5,8 @@ PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    text = input(f'Input the text: ')
+    s.sendall(text.encode())
     data = s.recv(1024)
 
 print('Received', repr(data))
