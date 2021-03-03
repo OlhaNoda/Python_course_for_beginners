31be145fd4b8e6881b3ce19fde69bc8541200417
onoda <olga.noda@gmail.com>
2021-03-03 21:51:26 +0200

    first version

--- 2021_03/lesson31_030321/task1_030321/task1_030321.py
+++ 2021_03/lesson31_030321/task1_030321/task1_030321.py
@@ -7,6 +7,9 @@ Download and save to file robots.txt from wikipedia, twitter websites etc.
 import requests
 
 url = 'https://uk.wikipedia.org/robots.txt'
-ret = requests.get(url)
-with open('uk_wikipedia_org_robots.txt', 'wb') as f:
-    f.write(ret.content)
+
+if __name__ == '__main__':
+    ret = requests.get(url)
+    if ret.ok:
+        with open('uk_wikipedia_org_robots.txt', 'wb') as f:
+            f.write(ret.content)
