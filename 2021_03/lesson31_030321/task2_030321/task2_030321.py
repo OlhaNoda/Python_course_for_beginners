e14c2b26a8ee241b394f24a04b006579c95b0d48
onoda <olga.noda@gmail.com>
2021-03-03 17:27:09 +0200

    first version

--- 2021_03/lesson31_030321/task2_030321/task2_030321.py
+++ 2021_03/lesson31_030321/task2_030321/task2_030321.py
@@ -25,6 +25,15 @@ def get_comments(url, author):
     return comments
 
 
+def set_key_sort_by_time(value):
+    return value['time']
+
+
+def sort_comments_by_time(comments):
+    comments.sort(key=set_key_sort_by_time)
+    return comments
+
+
 def write_data_to_file(data):
     with open('comments.json', 'w') as f:
         json.dump(data, f)
@@ -33,4 +42,8 @@ def write_data_to_file(data):
 if __name__ == '__main__':
     my_url = "https://api.pushshift.io/reddit/comment/search/"
     my_author = "YogaG5"
-    write_data_to_file(get_comments(my_url, my_author))
+    my_comments = get_comments(my_url, my_author)
+    sorted_comments = sort_comments_by_time(my_comments)
+    write_data_to_file(sorted_comments)
+    for comment in sorted_comments:
+        print(comment)
