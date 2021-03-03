4b79895ca9dbcf84335a587fec46423b87688211
onoda <olga.noda@gmail.com>
2021-03-03 16:43:26 +0200

    first version

--- 2021_03/lesson31_030321/task2_030321/task2_030321.py
+++ 2021_03/lesson31_030321/task2_030321/task2_030321.py
@@ -7,12 +7,30 @@ As a result, store all comments in chronological order in JSON and dump it to a 
 
 import requests
 import json
+import datetime
 
-url = "https://api.pushshift.io/reddit/comment/search/"
-author = "YogaG5"
 
-response = requests.get(url, {"author": author})
-data = response.json()
+def transform_unix_time(unix_time):
+    value = datetime.datetime.fromtimestamp(unix_time)
+    return value.strftime('%Y-%m-%d %H:%M:%S')
 
-with open('comments.json', 'w') as f:
-    json.dump(data['data'], f)
+
+def get_comments(url, author):
+    response = requests.get(url, {"author": author})
+    data = response.json()
+    comments = []
+    for entry in data['data']:
+        comment = {'time': transform_unix_time(entry['created_utc']), 'text': entry['body']}
+        comments.append(comment)
+    return comments
+
+
+def write_data_to_file(data):
+    with open('comments.json', 'w') as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    my_url = "https://api.pushshift.io/reddit/comment/search/"
+    my_author = "YogaG5"
+    write_data_to_file(get_comments(my_url, my_author))
