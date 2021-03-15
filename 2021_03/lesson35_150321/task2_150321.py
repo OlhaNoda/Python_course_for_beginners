bea77e3856d6e1f0f32393d48de3720b32d4a85a
onoda <olga.noda@gmail.com>
2021-03-15 17:12:46 +0200

    first version

---
+++ 2021_03/lesson35_150321/task2_150321.py
@@ -0,0 +1,44 @@
+# task2_150321
+"""
+Requests using asyncio and aiohttp
+Download all comments from a subreddit of your choice using URL: https://api.pushshift.io/reddit/comment/search/ .
+As a result, store all comments in chronological order in JSON and dump it to a file.
+For this task use asyncio and aiohttp libraries for making requests to Reddit API.
+"""
+import json
+import aiohttp
+import asyncio
+
+
+async def load(url, subreddit):
+    session = aiohttp.ClientSession()
+    resp = await session.get(url, params={"subreddit": subreddit})
+    data = await resp.json()
+    await session.close()
+    return data
+
+
+def get_content(url, subreddits):
+    content = []
+    loop = asyncio.get_event_loop()
+    for subreddit in subreddits:
+        data = loop.run_until_complete(load(url, subreddit))
+        content.append(data)
+    loop.close()
+    return content
+
+
+def write_data_to_file(data):
+    with open('comments.json', 'w') as f:
+        json.dump(data, f)
+
+
+def main(url, subreddits):
+    data = get_content(url, subreddits)
+    write_data_to_file(data)
+
+
+if __name__ == '__main__':
+    my_url = "https://api.pushshift.io/reddit/comment/search/"
+    my_subreddits = ("Pikabu", "distantsocializing")
+    main(my_url, my_subreddits)
