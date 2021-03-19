5a4d453210d4b0258c4d519d5ff5bd33e899e847
onoda <olga.noda@gmail.com>
2021-03-19 22:33:00 +0200

    first version

---
+++ 2021_03/lesson37_200321/task1_200321.py
@@ -0,0 +1,44 @@
+# task1_200321
+"""
+Create a table
+Create a table of your choice inside the sample SQLite database, rename it, and add a new column.
+Insert a couple rows inside your table. Also, perform UPDATE and DELETE statements on inserted rows.
+As a solution to this task, create a file named: task1.sql, with all the SQL statements you have used to accomplish this task
+"""
+import sqlite3
+
+# установка связи с базой данных
+conn = sqlite3.connect('example.db')
+cur = conn.cursor()
+
+# создание таблицы users
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INT PRIMARY KEY,
+   fname TEXT,
+   lname TEXT,
+   gender TEXT);
+""")
+conn.commit()
+
+# добавление нового поля
+cur.execute("ALTER TABLE users ADD COLUMN 'age' 'INT'")
+conn.commit()
+
+
+# добавление записи в таблицу users
+cur.execute("""INSERT INTO users(userid, fname, lname, gender, age) 
+   VALUES('00003', 'Tom', 'Jackson', 'male', '25');""")
+conn.commit()
+
+
+# корректировка записи
+cur.execute("""UPDATE users SET age = '30' WHERE userid = '00001'""")
+conn.commit()
+
+# удаление записи
+cur.execute("""DELETE FROM users WHERE lname = 'Jhonson'""")
+conn.commit()
+
+# вывод таблицы users
+for value in cur.execute("SELECT * FROM users"):
+    print(value)
